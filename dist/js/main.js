18e2ba2e15d54fb7d100d2b12b0179c0a57bb7d1
Momen Ellaban <68505187+MomenEllaban@users.noreply.github.com>
2024-05-19 09:38:36 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,338 @@
+var arrlang = {
+  en: {
+    Qsalary_title: "Your Cash Solution - Qsalary",
+    // navbar
+    Your_Cash_Solution: "| Your Cash Solution",
+    Home: "Home",
+    About_Us: "About Us",
+    Employer_Benefits: "",
+    Employer_Benefits: "Employer Benefits",
+    Employee_Benefits: "Employee Benefits",
+    Contact: "Contact",
+    Join_Today: "Join Today",
+    // 
+    Financial: `Financial Wellness Gate for Your Employees`,
+    Empower: "Empower your employees to access their earned wages anytime, and make every day a payday.",
+    Request_a_Demo: "Request a Demo",
+    Download: "Download the APP ",
+    Help_Us: "Help Us Contact Your Company",
+    // first
+    Employer_Benefits: "Employer Benefits",
+    Zero_Cost: "Zero Cost",
+    Zero_Cost_p: "No hidden, surprise or additional fees.",
+    Zero_Interest: "Zero Interest",
+    Zero_Interest_p: "Full compliance with Shariah.",
+    Time_saving: "Time-saving",
+    Time_savin_p: "Release pressure on human resources in your company.",
+    Easy_Liquidity: "Easy Liquidity",
+    Easy_Liquidity_p: "Your employees will no longer need to apply for loans.",
+    Money_Collection: "Money Collection",
+    Money_Collection_p:
+      "Withdrawn Payments will be automatically deducted when transferring salaries.",
+    Quick_Access: "Quick Access",
+    Quick_Access_p:
+      "Meet the financial needs of your  employees whenever they want.",
+    Increased_Productivity: "Increased Productivity",
+    Increased_Productivity_p:
+      "Free your employees from financial worries, enabling complete focus on their work.",
+    Turnover_Reduction: "Turnover Reduction",
+    Turnover_Reduction_p: "Enhance employee job satisfaction and loyalty",
+    See_more: "See more",
+    //
+    How_Does_Qsalary_Work: "How Does Qsalary Work?",
+    How_Does_Qsalary_Work_p_1: "Financial Freedom for <span class='nowrap'> Your Employees </span>",
+    How_Does_Qsalary_Work_p_2: ` in 3 Simple Steps`,
+    Register: "Register Your Company",
+    Register_p:
+      "Registration is easy, complications and hassle free, just contact us.",
+    Add_Employees: "Add Your Employees",
+    Add_Employees_p:
+      "Effortlessly add your employees and their details in a click or let us take care of it.",
+    Set_Employees: "Set Maximum Withdrawal Limits for Each Employee",
+    Set_Employees_p:
+      "Enable easy on-demand access to earned wages, complying with wage protection laws.",
+    //
+    Taking_Employees_1: "Taking your employees out ",
+    Taking_Employees_2: "of their financial stress",
+    n72: "72%",
+    p_72: "of individuals experience financial stress at least once a year",
+    // n300: `300<sup>k</sup> $`,
+    p_300:
+      "the annual cost to employers in lost productivity as a result of employee financial stress",
+    n20: "20%",
+    p_20: "of employee turnover attributable to financial stress",
+    REQUEST_A_DEMO: "REQUEST A DEMO",
+    //
+    Employee_Benefits: "Employee Benefits",
+    Instant: "Instant Liquidity 24/7",
+    Instant_p:
+      "Do you have emergency expenses? Get liquidity with just one click.",
+    Wages: "No More Wages Delays",
+    Wages_p:
+      "Administrative procedures will no longer hinder your earned wages access. Get the money you need anytime.",
+    Promotion: `You Won't Miss Any Promotion`,
+    Promotion_p:
+      "With Qsalary, never miss sales discounts due to liquidity issues.",
+    Centralized: "Centralized Expense Management",
+    Centralized_p:
+      "Enjoy a user-friendly app displaying your remaining wages on your smartphone.",
+    Interest: "Fixed Fees, 0% Interest",
+    Interest_p:
+      "No interest will apply to any amount you withdraw, but a flat fee per withdrawal.",
+    //
+    FAQs: "FAQs",
+    What_is_Qsalary: "What is Qsalary?",
+    Qsalary_service: "Is Qsalary a loan service?",
+    minimum_number_of_employees:
+      "Is there a minimum number of employees required from the company ?",
+    whom_Qsalary: "For whom is Qsalary?",
+    minimum_salary_requirement: "Is there a minimum salary requirement for this app?",
+    Qsalary_charge: "Does Qsalary charge any interest?",
+
+    q1: `Qsalary is an advanced and secure app that provides
+    employees with access to their earned wages anytime. This
+    offers them the flexibility to manage financial matters
+    and unexpected expenses between pay periods, making every
+    day a payday.`,
+    q2: "No loans involved – we give employees direct access to their own earned salary!    ",
+    q3: "No, there isn’t a minimum number of employees required to join.    ",
+    q4: "There is no minimum salary to benefit from the application.",
+    q5: "No, Qsalary doesn’t charge any interest.",
+
+
+    // 
+    Success_Partners: "Our Success Partners",
+    //
+    Start_Now: "Start Now",
+
+    empower_your_employees_1: 'Ready to empower your employees with  ',
+    empower_your_employees_2: '    anytime wage access?',
+
+
+    Quick: "Quick & easy",
+    Automation: "Automation",
+    no_Interests: "No Interests",
+    Stress_Free: "Stress Free",
+    //
+    Stay_in_the_know: "Stay in the know",
+    Subscribe_Mailing: "Subscribe Mailing List",
+    Follow_Us: "Follow Us",
+    Social_Media_Accounts: "Social Media Accounts",
+    Subscribe: "Subscribe",
+
+    //
+    Blog: "Blog",
+    FAQ: "FAQ",
+    Privacy_Policy: "Privacy Policy",
+    arbic: "العربية",
+    // footer
+    Powered: "Powered by ASFA Ventures",
+    rights: "All rights reserved© Qsalary 2023",
+  },
+  ar: {
+    Home: "الرئيسية",
+    Qsalary_title: "كيو سلاري، سيولة بكل سهولة - Qsalary",
+    // navbar
+    Your_Cash_Solution: "| سيولة بكل سهولة",
+    About_Us: "نبذة عنا",
+    Employer_Benefits: "مزايا صاحب العمل",
+    Employee_Benefits: "مزايا الموظفين",
+    Contact: "تواصل معنا",
+    Join_Today: "انضم الآن",
+    // 
+    Financial: `كيوسالاري، البوابة المالية لتوفير الراحة لموظفيك`,
+    Empower: "مكِّن موظفيك من الوصول إلى مستحقاتهم المالية بسهولة وفي أي وقت، ليكون كل يوم هو يوم الراتب.",
+    Request_a_Demo: "اطلب عرضاً توضيحياً الآن",
+    Download: "حـمّـل الـتطبـيـق",
+    Help_Us: "واحصل على السيولة بسهولة ",
+
+    // first
+    Zero_Cost: "صفر تكلفة ",
+    Zero_Cost_p: "بدون رسوم خفية أو مفاجئة أو إضافية.    ",
+    Zero_Interest: "صفر فوائد",
+    Zero_Interest_p: "متوافق مع مبادئ الشريعة الإسلامية.",
+    Time_saving: "توفير الوقت    ",
+    Time_savin_p: "يخفف أعباء قسم الموارد البشرية في شركتك.",
+    Easy_Liquidity: "سيولة بسهولة",
+    Easy_Liquidity_p: "لن يحتاج موظفوك للاستدانة بعد الآن.",
+    Money_Collection: "استرداد المال    ",
+    Money_Collection_p:
+      "اقتطاع تلقائي للمبالغ المسحوبة عند تحويل الرواتب.      ",
+    Quick_Access: "وصول سريع    ",
+    Quick_Access_p:
+      "تأمين الاحتياجات المالية لموظفيك متى أرادوا.      ",
+    Increased_Productivity: "إنتاجية أعلى    ",
+    Increased_Productivity_p:
+      "إزالة الصعوبات المالية عن موظفيك ليكون تركيزهم على عملهم فقط.      ",
+    Turnover_Reduction: "الحد من التسرب الوظيفي    ",
+    Turnover_Reduction_p: "تحسين الرضا والولاء الوظيفي لموظفيك.    ",
+    See_more: "شـاهد الـمـزيـد",
+    //
+    How_Does_Qsalary_Work: "كيف يعمل تطبيق Qsalary؟    ",
+    How_Does_Qsalary_Work_p_1: " حرر موظفيك مالياً بـ 3 <span class='nowrap'>خطوات بسيطة </span> ",
+    How_Does_Qsalary_Work_p_2: `   `,
+    Register: "سجل شركتك    ",
+    Register_p:
+      "مع Qsalary ستكون عملية التسجيل سهلة وبسيطة، من غير أي تعقيدات أو عراقيل، فقط تواصل معنا.      ",
+    Add_Employees: "أضف موظفيك    ",
+    Add_Employees_p:
+      "أضف موظفيك وسجل بياناتهم بنقرات بسيطة، أو اترك لنا هذه المهمة.      ",
+    Set_Employees: "اختر الحد الأقصى للسحب لكل موظف",
+    Set_Employees_p:
+      "امنح موظفيك إمكانية الحصول على مستحقاتهم بسهولة بما يتوافق مع قوانين حماية الأجور. ",
+    //
+    Taking_Employees_1: "خَلّص موظفيك من قلق الضغوطات المالية ",
+    Taking_Employees_2: " ",
+    n72: "72%",
+    p_72: "من الأفراد يمرون بصعوبات وضائقات مالية مرة سنوياً على الأقل ",
+    p_300:
+      "هي الكلفة السنوية للخسارة بسبب نقص إنتاجية الموظفين الذين يعانون من ضغوطات مالية ",
+    n20: "20%",
+    p_20: "من نسب استقالة الموظفين ترجع إلى الضغوطات المالية  ",
+    REQUEST_A_DEMO: "اطـلـب عرضـاً توضـيـحـيـاً الـآن",
+    //
+    Instant: "سيولة مالية فورية 24/7 ",
+    Instant_p:
+      "هل أنت بحاجة إلى مصاريف طارئة؟ احصل على السيولة بضغطة زر.",
+    Wages: "لا تأخير في صرف مستحقاتك    ",
+    Wages_p:
+      "لن تشكل الإجراءات الإدارية عائقاً للحصول على مستحقاتك المالية.",
+    Promotion: `لن يفوتك أي عرض ترويجي    `,
+    Promotion_p:
+      "لن تكون نقص السيولة سبباً في تفويت أي خصم على الشراء بعد الآن.",
+    Centralized: "إدارة مصاريفك من مكان واحد",
+    Centralized_p:
+      "تمتع بتطبيق بسيط وسهل يُظهر المبلغ المتبقي من مستحقاتك من خلال هاتفك الذكي.",
+    Interest: "رسوم ثابتة، 0% فوائد",
+    Interest_p:
+      "لن تُطبَّق أية فوائد على أي مبلغ تقوم بسحبه، وإنَّما رسوم ثابتة لكل عملية سحب.",
+    //
+    FAQs: "الأسئلة الشائعة",
+    What_is_Qsalary: "ما هو تطبيق Qsalary؟",
+    Qsalary_service: "هل Qsalary خدمة قروض؟",
+    whom_Qsalary: "لمن تطبيق Qsalary؟",
+
+    Qsalary_charge: "هل يوجد حد أدنى لعدد الموظفين في الشركة؟",
+
+    minimum_salary_requirement: "هل يوجد حد أدنى للرواتب المؤهلة للاستفادة من هذا التطبيق؟",
+
+    q1: "Qsalary هو تطبيق ذو خصائص متطورة وآمنة يمنح الموظفين إمكانية الوصول إلى مستحقاتهم في أي وقت من الشهر، بحيث يمنحهم المرونة في التحكم في شؤونهم المالية والمصاريف غير المتوقعة على مدار الشهر، ليجعل كل يوم هو يوم الراتب.",
+    q2: "لا، Qsalary لا يمنح قروضاً؛ بل يمنح الموظفين إمكانية الوصول المباشر إلى مستحقاتهم المالية.",
+    q3: "Qsalary هو لكل صاحب عمل يرغب بتمكين موظفيه وزيادة رضاهم الوظيفي، ولكل موظف يريد الحصول على سيولة مالية بسهولة.",
+    q4: "لا يوجد حد أدنى للرواتب للاستفادة من التطبيق.",
+    q5: "لا، ليس هنالك حد أدنى لعدد الموظفين المطلوبين للانضمام إلى الخدمة.",
+
+
+
+
+
+
+
+
+
+    // 
+    Success_Partners: "شركاء النجاح    ",
+    //
+    Start_Now: "ابدأ الآن    ",
+    empower_your_employees_1: 'جاهز لتمكين موظفيك ',
+    empower_your_employees_2: 'من الوصول إلى رواتبهم المستحقة متى أرادوا؟ ',
+    Quick: "سهل وسريع    ",
+    Automation: "بشكل آلي    ",
+    no_Interests: "دون فوائد    ",
+    Stress_Free: "خالي من الضغوطات    ",
+    //
+    Stay_in_the_know: "ابقى على اطلاع    ",
+    Subscribe_Mailing: "اشترك بالنشرة البريدية    ",
+    Follow_Us: "تابعنا على    ",
+    Social_Media_Accounts: "منصات التواصل الإجتماعي    ",
+    Subscribe: "إشتراك",
+    //
+    Blog: "المدونة",
+    FAQ: "الأسئلة الشائعة ",
+    Privacy_Policy: " سياسة الخصوصية",
+    arbic: "العربية",
+    // footer
+    Powered: "Powered by ASFA Ventures",
+    rights: "جميع الحقوق محفوظة © Qsalary 2023",
+  },
+};
+
+
+$(document).ready(function () {
+  function applyStyles(lang, theme) {
+    let direction = lang === "ar" ? "rtl" : "ltr";
+    let textAlign = lang === "ar" ? "right" : "left";
+    $("body").css({ "direction": direction, "text-align": textAlign });
+    $(".owl-carousel").owlCarousel({ rtl: true, });
+    $(".lang").css("direction", direction);
+    $(".translate").attr("id", lang === "ar" ? "en" : "ar").html(lang === "ar" ?
+      `<img src="dist/img/en.svg" alt="" width="20px"> English</button>` :
+      `<img src="dist/img/soudia.png" alt="" width="20px"> العربية</button>`);
+    $(".translate").attr("id") == 'ar' ?
+      $("body").css("font-family", "Montserrat") :
+      $("body").css("font-family", "IBMPlexSansArabic");
+    $(".translate_btn").attr("id") == 'ar' ?
+      $(".translate_btn").css("font-family", "IBMPlexSansArabic") :
+      $(".translate_btn").css("font-family", "Montserrat");
+    // console.log(lang)
+
+    lang === "ar" ? $("#i__phone").attr("src", 'dist/img/Floating-iPhone-Ar-2-min.png') : $("#i__phone").attr("src", 'dist/img/Floating-iPhone-En-2-min.png');
+    lang === "ar" ? $(".splash_bg2").css("background-image", 'url(dist/img/left__bg.png)') :
+      $(".splash_bg2").attr("background-image", 'url(./dist/img/right__bg.png)');
+
+    lang === "en" ? $(".splash_bg2").css("background-image", 'url(dist/img/right__bg.png)') :
+      $(".splash_bg2").attr("background-image", 'url(dist/img/left__bg.png)');
+
+
+  }
+
+  function updateLanguage(lang) {
+    localStorage.setItem("langu", lang);
+    let locallang = localStorage.getItem("langu");
+    $(".lang").each(function () {
+      if (arrlang && arrlang[locallang]) {
+        $(this).html(arrlang[locallang][$(this).attr("key")]);
+      }
+    });
+    applyStyles(locallang);
+  }
+
+  $(".translate").click(function () {
+    let lang = $(this).attr("id");
+    updateLanguage(lang);
+  });
+
+  let storedLang = localStorage.getItem("langu");
+  if (storedLang) {
+    updateLanguage(storedLang);
+  }
+
+});
+
+
+// start counter number
+let nums = document.querySelectorAll(".nums .num");
+let section = document.querySelector(".three");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - window.innerHeight) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+// end counter number
+
